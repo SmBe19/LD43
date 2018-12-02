1822313879170dd1524d2202c56ccf7fc19aa09c
Benjamin Schmid <git@schmidb.ch>
2018-12-02 21:15:52 +0100

    mute

--- scn/ECGBox.gd
+++ scn/ECGBox.gd
@@ -1,10 +1,13 @@
 extends Sprite
 
+var muted = false
 var should_beep = false
+var is_flat = false
 
 func randomized_start(animation):
 	$Flat.stop()
 	should_beep = false
+	is_flat = false
 	$ECG.play(animation)
 	$ECG.frame = randi()%$ECG.frames.get_frame_count(animation)
 	var speed = 10 + randf() * 15
@@ -16,13 +19,24 @@ func alive():
 
 func flat():
 	self.randomized_start("flat")
+	is_flat = true
 	$Flat.play()
 
 func off():
 	self.randomized_start("off")
 
 func _process(delta):
-	if should_beep:
+	if should_beep and not muted:
 		if $ECG.frame == 5:
 			if not $Beep.playing:
 				$Beep.play()
+
+func _on_MuteButton_toggled(button_pressed):
+	muted = button_pressed
+	if muted:
+		$Flat.stop()
+		$Beep.stop()
+	else:
+		if is_flat:
+			$Flat.play()
+	
